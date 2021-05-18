c93a2edd681f315c2859a593f093a9a1ef0031bd
Ismet Kadyrov <Kadyrovismet1999@gmail.com>
2021-05-18 19:03:11 +0500

    add req

--- src/auth/jwt/useJwt.js
+++ src/auth/jwt/useJwt.js
@@ -1,5 +1,19 @@
 import useJwt from '@core/auth/jwt/useJwt'
 import axios from '@axios'
 
-const { jwt } = useJwt(axios, {})
+const { jwt } = useJwt(axios, {
+    // Endpoints
+    loginEndpoint: '/jwt/login',
+    registerEndpoint: '/jwt/register',
+    refreshEndpoint: '/jwt/refresh-token',
+    logoutEndpoint: '/jwt/logout',
+
+    // This will be prefixed in authorization header with token
+    // e.g. Authorization: Bearer <token>
+    tokenType: 'Bearer',
+
+    // Value of this property will be used as key to store JWT token in storage
+    storageTokenKeyName: 'accessToken',
+    storageRefreshTokenKeyName: 'refreshToken',
+})
 export default jwt
